82289a4df1a160d4c1c8b5a14ff5c2a1d65c20fe
liumingyu <798511784@qq.com>
2020-08-11 17:40:18 +0800

    添加FirstClass11

--- com.cmft.git/src/FirstClass.java
+++ com.cmft.git/src/FirstClass.java
@@ -2,7 +2,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("1111111111111111");
         //哈哈哈哈
-        //你好~
+        //你好~111
     }
 
     public void test(){
