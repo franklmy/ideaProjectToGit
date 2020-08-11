d556de0d68e6be499ca258a324c433003d8da9f4
liumingyu <798511784@qq.com>
2020-08-11 17:29:32 +0800

    从远程仓库拷贝源代码，修改后再次push

--- com.cmft.git/src/FirstClass.java
+++ com.cmft.git/src/FirstClass.java
@@ -2,6 +2,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("1111111111111111");
         //哈哈哈哈
+        //你好~
     }
 
     public void test(){
